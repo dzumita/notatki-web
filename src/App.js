fef3d90aa4e8c949dec50482c660d9829ca6129e
Dzumita <dzumitanium@gmail.com>
2022-03-26 23:04:49 +0300

    add client

--- src/App.js
+++ src/App.js
@@ -1,72 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  ApolloClient,
-  ApolloProvider,
-  createHttpLink,
-  InMemoryCache
-} from '@apollo/client';
-import { setContext } from 'apollo-link-context';
+import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from 'styled-components';
 
-import Routes from '/Routes';
-import GlobalStyle from '/components/GlobalStyle';
+import Routes from './Routes';
+import GlobalStyle from './components/GlobalStyle';
+import theme from './theme';
+import client from './client';
 
-const uri = process.env.API_URI;
-const httpLink = createHttpLink({ uri });
-const cache = new InMemoryCache();
-
-const authLink = setContext((_, { headers }) => {
-  return {
-    headers: {
-      ...headers,
-      authorization: localStorage.getItem('token') || ''
-    }
-  };
-});
-
-const theme = {
-  colors: {
-    background: '#282a36',
-    foreground: '#f8f8f2',
-    currentLine: '#44475a',
-    comment: '#6272a4',
-    cyan: '#8be9fd',
-    green: '#50fa7b',
-    orange: '#ffb86c',
-    pink: '#ff79c6',
-    purple: '#bd93f9',
-    red: '#ff5555',
-    yellow: '#f1fa8c'
-  },
-  border: {
-    radius: '12px'
-  }
-};
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache,
-  resolvers: {},
-  connectToDevTools: true
-});
-
-const data = {
-  isLoggedIn: !!localStorage.getItem('token')
-};
-
-cache.writeData({ data });
-client.onResetStore(() => cache.writeData({ data }));
-
-const App = () => {
-  return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Routes />
-      </ThemeProvider>
-    </ApolloProvider>
-  );
-};
+const App = () => (
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Routes />
+    </ThemeProvider>
+  </ApolloProvider>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/client.js
@@ -0,0 +1,35 @@
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache
+} from '@apollo/client';
+import { setContext } from 'apollo-link-context';
+
+const uri = process.env.API_URI;
+const httpLink = createHttpLink({ uri });
+const cache = new InMemoryCache();
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || ''
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache,
+  resolvers: {},
+  connectToDevTools: true
+});
+
+const data = {
+  isLoggedIn: !!localStorage.getItem('token')
+};
+
+cache.writeData({ data });
+client.onResetStore(() => cache.writeData({ data }));
+
+export default client
