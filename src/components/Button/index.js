78d7dc9bcf2a7a94b7709662f7745be9573a5991
Dzumita <dzumitanium@gmail.com>
2022-03-26 20:43:02 +0300

    button color fix

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,7 +4,7 @@ const Button = styled.button`
   border-radius: ${props => props.theme.border.radius};
   border: none;
   background-color: ${props => props.theme.colors.purple};
-  color: ${props => props.theme.colors.foreground};
+  color: ${props => props.theme.colors.background};
   font-weight: bold;
   text-transform: uppercase;
   font-size: 1em;
