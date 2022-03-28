4495c414a8365d59145a675fddad7d0a7d5d4c33
Dzumita <dzumitanium@gmail.com>
2022-03-28 15:34:20 +0300

    add logic for notes

--- src/routes/notes/index.jsx
+++ src/routes/notes/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
 
-const Notes = () => (
-  <div>
-    <p>These are my notes</p>
-  </div>
-);
+import StatusItem from '../../components/StatusItem';
+import Note from '../../components/Note';
+import { GET_MY_NOTES } from '../../gql/queries';
 
-export default Notes;
+import { Wrapper } from './styles';
+
+const MyNotes = () => {
+  const { loading, error, data } = useQuery(GET_MY_NOTES);
+
+  if (loading) return <StatusItem as="load" />;
+  if (error) return <StatusItem />;
+
+  if (data.me.notes.length !== 0) {
+    return (
+      <Wrapper>
+        {data.me.notes.map(note => (
+          <Note note={note} key={note.id} />
+        ))}
+      </Wrapper>
+    );
+  } else {
+    return <Wrapper>No notes yet</Wrapper>;
+  }
+};
+
+export default MyNotes;
