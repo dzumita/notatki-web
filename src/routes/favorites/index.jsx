70c4bd820b787514954ef1b6199d86d36926760a
Dzumita <dzumitanium@gmail.com>
2022-03-28 15:40:18 +0300

    update favorite page

--- src/routes/favorites/index.jsx
+++ src/routes/favorites/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
 
-const Favorites = () => (
-  <div>
-    <p>These are my Favorites</p>
-  </div>
-);
+import StatusItem from '../../components/StatusItem';
+import Note from '../../components/Note';
+import { GET_MY_FAVORITES } from '../../gql/queries';
+
+import { Wrapper } from './styles';
+
+const Favorites = () => {
+  const { loading, error, data } = useQuery(GET_MY_FAVORITES);
+
+  if (loading) return <StatusItem as="load" />;
+  if (error) return <StatusItem />;
+
+  if (data.me.favorites.length !== 0) {
+    return (
+      <Wrapper>
+        {data.me.favorites.map(note => (
+          <Note note={note} key={note.id} />
+        ))}
+      </Wrapper>
+    );
+  } else {
+    return <Wrapper>No favorites yet</Wrapper>;
+  }
+};
 
 export default Favorites;
